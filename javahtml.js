d2b096adfc2a00185189f7dbdadf9d733e2c1cca
Ismael <spxoki@gmail.com>
2022-02-21 08:33:43 +0100

    Magic Ball

---
+++ javahtml.js
@@ -0,0 +1,33 @@
+const go$red = document.getElementById('red')
+const go$yellow = document.getElementById('yellow')
+const go$purple = document.getElementById('purple')
+const go$green = document.getElementById('green')
+
+
+
+function gored() {
+  document.getElementById('balls').style.backgroundColor = 'red'
+}
+function goyellow() {
+  document.getElementById('balls').style.backgroundColor = 'yellow'
+}
+function gopurple() {
+  document.getElementById('balls').style.backgroundColor = 'purple'
+}
+function gogreen() {
+  document.getElementById('balls').style.backgroundColor = 'green'
+}
+
+
+function gomo() {
+  document.getElementById('balls').style.borderColor = 'brown'
+}
+function goma() {
+  document.getElementById('balls').style.borderColor = 'orange'
+}
+function gome() {
+  document.getElementById('balls').style.borderColor = 'grey'
+}
+function gomk() {
+  document.getElementById('balls').style.borderColor = 'white'
+}
